907c15fd61bc914d36f99fb5cd4975c5e7df97b8
regasj2018 <jregas@sjnma.org>
2018-02-05 10:19:51 -0600

    update good

---
+++ variables.py
@@ -0,0 +1,21 @@
+print("hello world!")
+print("hello world!")
+print("""ansvar
+vad
+fvas
+vas
+v
+asdv
+Hopefully this will show up
+""")
+
+first_name = raw_input("what is your first name")
+
+last_name = raw_input("what is your last name")
+
+full_name = first_name +" "+last_name
+
+print("yes this works, "+full_name)
+
+
+
